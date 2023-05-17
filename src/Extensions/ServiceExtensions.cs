66ebf119d55125dd9e58872bb20af8278fb82322
alexlvovich <alexander@lvovi.ch>
2023-05-17 17:28:55 +0200

    adding service extension

---
+++ src/Extensions/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Konso.Clients.ValueTracking.Interfaces;
+using Konso.Clients.ValueTracking.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Konso.Clients.ValueTracking.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddKonsoValueTracking(this IServiceCollection services)
+        {
+            // use httpclient factory
+            services.AddHttpClient();
+            services.AddSingleton<IValueTrackingClient, ValueTrackingClient>();
+        }
+    }
+}
